3d7d0f70ab625a56cef32c03d1a1e0aaff324625
Bastian2704 <sebastianabad2704@gmail.com>
2024-12-02 10:37:50 -0500

    Creacion Main Page

--- AbadExamenP2/ASMainPage.xaml.cs
+++ AbadExamenP2/ASMainPage.xaml.cs
@@ -2,7 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
 
         public MainPage()
         {
@@ -11,15 +11,18 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            Label Resultado = new Label();
+            Resultado.SetBinding(Label.TextProperty, new Binding("Resultado", source: resultado));
+            int valor = Int32.Parse("45");
+            var picker1 = (Picker)sender;
+            var picker2 = (Picker)sender;
+            int pick1 = picker1.SelectedIndex;
+            int pick2 = picker2.SelectedIndex;
+            if (pick1 == 0 && pick2 == 0)
+            {
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            }
         }
-    }
 
+    }
 }
