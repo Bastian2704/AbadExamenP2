3afd8aef80f16e7e8928b721239aeed6865a2d62
Bastian2704 <sebastianabad2704@gmail.com>
2024-12-02 09:29:44 -0500

    Agregar archivos de proyecto.

---
+++ AbadExamenP2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AbadExamenP2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
