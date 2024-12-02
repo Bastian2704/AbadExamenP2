265d51198436032f59a4ebe2778991b706d48ea9
Bastian2704 <sebastianabad2704@gmail.com>
2024-12-02 11:09:07 -0500

    Casi

--- AbadExamenP2/ASMainPage.xaml.cs
+++ AbadExamenP2/ASMainPage.xaml.cs
@@ -3,6 +3,8 @@
     public partial class MainPage : ContentPage
     {
 
+        int selectedIndex1;
+        int selectedIndex2;
 
         public MainPage()
         {
@@ -11,16 +13,39 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
+            double valor = Double.Parse(numero.Text);
             Label Resultado = new Label();
-            Resultado.SetBinding(Label.TextProperty, new Binding("Resultado", source: resultado));
-            int valor = Int32.Parse("45");
-            var picker1 = (Picker)sender;
-            var picker2 = (Picker)sender;
-            int pick1 = picker1.SelectedIndex;
-            int pick2 = picker2.SelectedIndex;
+            Resultado.SetBinding(Label.TextProperty, new Binding("R", source: resultado));
+
+            int pick1 = selectedIndex1;
+            int pick2 = selectedIndex2;
+
+            Resultado.Text = valor.ToString("F2");
+       
+          //  Resultado.Text = "valor.ToString()";
             if (pick1 == 0 && pick2 == 0)
             {
+                
+            }
+        }
+        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker1 = (Picker)sender;
+            int selectedIndex = picker1.SelectedIndex;
 
+            if (selectedIndex != -1)
+            {
+               selectedIndex1 = picker1.SelectedIndex;
+            }
+        }
+        void OnPickerSelectedIndexChanged2(object sender, EventArgs e)
+        {
+            var picker2 = (Picker)sender;
+            int selectedIndex = picker2.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                selectedIndex1 = picker2.SelectedIndex;
             }
         }
 
